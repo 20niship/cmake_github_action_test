2d44f0a2d16301f1cfb9d3572f6a119fc9f39fbf
20niship <idcnishimiya5378@gmail.com>
2023-07-08 14:48:17 +0900

    global_var_test

--- cmake/test_global_bar.cmake
+++ cmake/test_global_bar.cmake
@@ -1,7 +1,7 @@
 
-set(TEST_GLOBAL_BAR "bar") 
+set(TEST_GLOBAL_BAR "foo") 
 
 function(add_test_global)
   set(TEST_GLOBAL_BAR "${TEST_GLOBAL_BAR} bar") 
-  message(STATUS "TEST_GLOBAL_FOO: ${TEST_GLOBAL_FOO}")
+  message(STATUS "TEST_GLOBAL_FOO: ${TEST_GLOBAL_BAR}")
 endfunction()
